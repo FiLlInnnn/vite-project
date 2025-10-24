ce8fbb7e6ba8f4dc7801a31574e3bb3e80927743
Hurmex <www.seznam666@seznam.cz>
2025-10-24 11:50:18 +0200

    my update

--- src/pages/Page1/index.tsx
+++ src/pages/Page1/index.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function index() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1>Header</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, libero similique dicta sint natus dolores quidem in facilis sed adipisci molestias beatae totam assumenda consequatur fuga fugit repellat est aliquam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quis expedita sapiente repellendus deserunt excepturi earum aperiam fugit ex, eveniet quidem sunt error ab ipsam hic illo assumenda saepe laudantium?</p>
+      <img src="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg" alt="" />
+    </>
   )
 }
